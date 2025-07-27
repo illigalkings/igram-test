9a38a5e81b13d6d1409ef2513570b3ef28776d10
illigalkings <illigalkings@proton.me>
2025-07-27 21:52:24 +0530

    Create AdComponent.jsx

---
+++ src/AdComponent.jsx
@@ -0,0 +1,24 @@
+import { useEffect } from "react";
+
+const AdComponent = () => {
+  useEffect(() => {
+    try {
+      (window.adsbygoogle = window.adsbygoogle || []).push({});
+    } catch (e) {
+      console.error("AdSense error", e);
+    }
+  }, []);
+
+  return (
+    <ins
+      className="adsbygoogle"
+      style={{ display: "block" }}
+      data-ad-client="ca-pub-2295806962223513"
+      data-ad-slot="7414611032"
+      data-ad-format="auto"
+      data-full-width-responsive="true"
+    ></ins>
+  );
+};
+
+export default AdComponent;
