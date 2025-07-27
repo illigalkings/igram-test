91244d63ae7536c3d4b2c4b244beb52f974b29f2
illigalkings <illigalkings@proton.me>
2025-07-27 21:31:15 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -211,6 +211,22 @@ const Hero = () => {
   );
 };
 
+// ads start
+
+<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2295806962223513"
+     crossorigin="anonymous"></script>
+<!-- main -->
+<ins class="adsbygoogle"
+     style="display:block"
+     data-ad-client="ca-pub-2295806962223513"
+     data-ad-slot="7414611032"
+     data-ad-format="auto"
+     data-full-width-responsive="true"></ins>
+<script>
+     (adsbygoogle = window.adsbygoogle || []).push({});
+</script>
+  
+// ads end
 
 // ===== Features Section =====
 function Features() {
